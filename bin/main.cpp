3228b6f573229fe0ac1471028bb9511c360203d2
FlyingMusic <1243050373@qq.com>
2021-07-15 14:18:47 +0800

    增加test_log指令参数输入

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,14 +1,49 @@
 #include "hlog.h"
 #include <time.h>    
 #include <stdio.h>    
+#include <getopt.h>
 #include "Config.h"
 
 #define LOG_TEST_MODEL "TEST_MODEL"
 
-int main() {
-    Config *p_config = new Config();
-    delete p_config;
-    p_config = NULL;
+const char* const short_options = "c:";
+struct option long_options[] = {
+    {"config", 1, NULL, 'c'},
+    {0, 0, 0, 0},
+};
+
+void Usage(const char *prog){
+    printf("Usage: %s -c ./log.config\n",prog);
+    return;
+}
+int main(int argc, char *argv[]) {
+    const char *config_path = NULL;
+    char c = 0;
+    while((c = getopt_long (argc, argv, short_options, long_options, NULL)) != -1 && c != 255){
+        switch (c){
+            case 'c':
+                config_path = optarg;
+                break;
+            case 'v':
+            case 'h':
+            case '?':
+                Usage(argv[0]);
+                exit(0);
+        }
+    }
+    if (NULL == config_path) {
+        Usage(argv[0]);
+        return 0;
+    }
+
+    Config *config = new Config();
+    config->load(config_path);
+    char debug_path[512] = {0};
+    int log_size = 0;
+    config->GetString("DEBUG_LOG_PATH", debug_path, "DEBUG_LOG_PATH");
+    config->GetValue("LOG_SIZE", log_size, 10);
+    delete config;
+    config = NULL;
 #if 0
     set_log_mode(NORMAL_MODE);
     set_log_level_file("normal.log", LEVEL_DEBUG, 10);
